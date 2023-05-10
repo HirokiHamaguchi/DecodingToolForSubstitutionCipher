2c1bd1b8aa4b586c8a7a60583266f1b7cc0b180e
hari64 <hari64boli64@gmail.com>
2023-05-11 08:46:04 +0900

    shorten the path

--- src/ts/imgPath.ts
+++ src/ts/imgPath.ts
@@ -1,8 +1,7 @@
-export const IMG_PATH_1 =
-  "https://github.com/hari64boli64/DecodingToolForSubstitutionCipher/blob/master/imgs/1.png?raw=true";
-export const IMG_PATH_2 =
-  "https://github.com/hari64boli64/DecodingToolForSubstitutionCipher/blob/master/imgs/2.png?raw=true";
-export const IMG_PATH_3 =
-  "https://github.com/hari64boli64/DecodingToolForSubstitutionCipher/blob/master/imgs/3.png?raw=true";
-export const IMG_PATH_4 =
-  "https://github.com/hari64boli64/DecodingToolForSubstitutionCipher/blob/master/imgs/4.png?raw=true";
+const PATH_HEAD =
+  "https://github.com/hari64boli64/DecodingToolForSubstitutionCipher/blob/master/imgs/";
+
+export const IMG_PATH_1 = PATH_HEAD + "1.png?raw=true";
+export const IMG_PATH_2 = PATH_HEAD + "2.png?raw=true";
+export const IMG_PATH_3 = PATH_HEAD + "3.png?raw=true";
+export const IMG_PATH_4 = PATH_HEAD + "4.png?raw=true";
